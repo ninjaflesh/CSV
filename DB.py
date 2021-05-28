15645d909c61228890216d46bacb5162fe8dc90e
Philip Graftio <49476875+ninjaflesh@users.noreply.github.com>
2021-05-28 15:18:28 +0300

    v 1.0

--- DB.py
+++ DB.py
@@ -1,10 +1,45 @@
 import sqlite3
 import bcrypt
+import openpyxl
 from time import gmtime, strftime
 
 # Подключение к БД
 with sqlite3.connect('server.db') as db:
     sql = db.cursor()
+
+sql.execute("""CREATE TABLE IF NOT EXISTS users (
+    user_id INTEGER PRIMARY KEY,
+    login TEXT,
+    password TEXT,
+    time TEXT
+)""")
+db.commit()
+
+sql.execute("""CREATE TABLE IF NOT EXISTS grp (
+    grp_id INTEGER PRIMARY KEY,
+    grp_name TEXT,
+    grp_num INTEGER
+)""")
+db.commit()
+
+sql.execute("""CREATE TABLE IF NOT EXISTS audience (
+    audience_id INTEGER PRIMARY KEY,
+    audience_name TEXT,
+    audience_num INTEGER,
+    audience_type NUMERIC
+)""")
+db.commit()
+
+sql.execute("""CREATE TABLE IF NOT EXISTS work (
+    work_id INTEGER PRIMARY KEY,
+    work_name TEXT,
+    work_type NUMERIC,
+    work_p TEXT NOT NULL
+)""")
+db.commit()
+
+# Подключение к книги Exel
+wb = openpyxl.load_workbook('schedule.xlsx')
 
 
 class User:  # Клас User, принемает логин и пароль
@@ -54,103 +89,206 @@ class User:  # Клас User, принемает логин и пароль
 
     # Метод позволяющий пользователю взаимодействовать с расписанием.
     def edit_schedule(self):
-        print("Для создания занятия введите: 1\nДля редактирования занятия введите: 2\n"
-              "Для удаления занятия введите: 3\nДля создания таблицы с расписанием введите: 4")
+        print("Для добавления группы введите: 1\nДля добавления аудитории введите: 2\n"
+              "Для добавления предмета введите: 3\nДля удаления группы введите: 4\n"
+              "Для удаления аудитории введите: 5\nДля удаления предмета введите: 6\n"
+              "Для создания расписания введите: 7")
         check = int(input('Выберите действие: '))
 
         if check == 1:
-            create_lesson()
+            create_group()
         elif check == 2:
-            edit_lesson()
+            create_audience()
         elif check == 3:
-            delete_lesson()
+            create_work()
         elif check == 4:
+            delet_group()
+        elif check == 5:
+            delet_audience()
+        elif check == 6:
+            delet_work()
+        elif check == 7:
             build_schedule()
         else:
             print("Я тебя не понимаю О_о")
 
 
-def create_lesson():
+def create_group():
     group = input('Введите название группы: ')
-    teacher = input('Введите ФИО преподователя: ')
-    work = input('Введите название предмета: ')
-    print("Если аудитория является классом, введите: 1\nЕсли аудитория является спортзалом, введите: 2\n"
-          "Если аудитория является лабораторией, введите: 3")
-    check = int(input('Введите тип аудитории: '))
-    audience = input('Введите номер аудитории: ')
+    group_num = input('Введите количество человек в группе: ')
 
-    if check == 1:
-        a_type = "Класс"
-    elif check == 2:
-        a_type = "Спортзал"
-    elif check == 3:
-        a_type = "Лаборатория"
-    else:
-        print("Я тебя не понимаю О_о")
-
-    sql.execute(f"INSERT INTO timetable VALUES (?, ?, ?, ?, ?, ?)",
-                (None, group, teacher, work, audience, a_type))
+    sql.execute(f"INSERT INTO grp VALUES (?, ?, ?)",
+                (None, group, group_num))
     db.commit()
-    print("Занятие создано.")
+    print(f"Добавлена группа {group}.")
 
 
-def edit_lesson():
-    sql.execute(f"SELECT * FROM timetable ")
+def create_audience():
+    audience = input('Введите номер аудитории: ')
+    audience_num = input('Введите вместимость адитории: ')
+    audience_type = input('Введите тип аудитории [0 - лаба, 1 - обычная]: ')
+
+    sql.execute(f"INSERT INTO audience VALUES (?, ?, ?, ?)",
+                (None, audience, audience_num, audience_type))
+    db.commit()
+    print(f"Добавлена аудитория №{audience}.")
+
+
+def create_work():
+    work = input('Введите название предмета: ')
+    work_type = input(
+        'Введите тип аудитории для проведения занятия [0 - лаба, 1 - обычная]: ')
+    work_p = input('Введите имя преподавателя: ')
+
+    sql.execute(f"INSERT INTO work VALUES (?, ?, ?, ?)",
+                (None, work, work_type, work_p))
+    db.commit()
+    print(f"Добавлен предмет {work}.")
+
+
+def delet_group():
+    sql.execute(f"SELECT * FROM grp ")
     for x in sql.fetchall():
         print(x)
-    id = int(input("Укажите id предмета для редакции: "))
-    print("Для изменения группы, введите: 1\nДля изменения препопдователя, введите: 2\n"
-          "Для изменения предмета, введите: 3\nДля изменения аудитории, введите 4\n"
-          "Для изменения номера аудитории, введите 5")
-    colom = int(input("Укажите номер колонны: "))
-    if colom == 1:
-        value = input("Введите новое название группы: ")
-        colom = "group"
-    elif colom == 2:
-        value = input("Введите новое имя преподователя: ")
-        colom = "teacher"
-    elif colom == 3:
-        value = input("Введите новое название предмета: ")
-        colom = "work"
-    elif colom == 4:
-        value = input("Введите новое название аудитории: ")
-        colom = "audience"
-    elif colom == 5:
-        print("Если аудитория является классом, введите: 1\nЕсли аудитория является спортзалом, введите: 2\n"
-              "Если аудитория является лабораторией, введите: 3")
-        check = int(input('Введите тип аудитории: '))
-        if check == 1:
-            value = "Класс"
-            colom = "type"
-        elif check == 2:
-            value = "Спортзал"
-            colom = "type"
-        elif check == 3:
-            value = "Лаборатория"
-            colom = "type"
-        else:
-            print("Я тебя не понимаю О_о")
-    else:
-        print("Я тебя не понимаю О_о")
-
-    sql.execute(
-        f"UPDATE timetable SET '{colom}'='{value}' WHERE timetable_id = '{id}'")
+    id = int(input("Укажите id группы для удаления: "))
+    sql.execute(f"DELETE FROM grp WHERE grp_id = '{id}'")
     db.commit()
-    print("Занятие отредактированно.")
+    print("Группа удалена.")
 
 
-def delete_lesson():
-    sql.execute(f"SELECT * FROM timetable ")
+def delet_audience():
+    sql.execute(f"SELECT * FROM audience ")
+    for x in sql.fetchall():
+        print(x)
+    id = int(input("Укажите id аудитории для удаления: "))
+    sql.execute(f"DELETE FROM audience WHERE audience_id = '{id}'")
+    db.commit()
+    print("Аудитория удалена.")
+
+
+def delet_work():
+    sql.execute(f"SELECT * FROM work ")
     for x in sql.fetchall():
         print(x)
     id = int(input("Укажите id предмета для удаления: "))
-    sql.execute(f"DELETE FROM timetable WHERE timetable_id = '{id}'")
+    sql.execute(f"DELETE FROM work WHERE work_id = '{id}'")
     db.commit()
-    print("Занятие удалено.")
+    print("Предмет удален.")
 
 
 def build_schedule():
-    None
+    sql.execute(
+        f"SELECT grp_name, grp_num FROM grp")
+    group = sql.fetchall()
+
+    sql.execute(
+        f"SELECT audience_name, audience_num, audience_type FROM audience")
+    audience = sql.fetchall()
+
+    sql.execute(
+        f"SELECT work_name, work_type FROM work")
+    work = sql.fetchall()
+
+    group.sort(key=lambda group: group[1], reverse=True)
+    audience = sorted(audience, key=lambda type: type[1], reverse=True)
+    audience.sort(key=lambda type: type[2])
+    work.sort(key=lambda type: type[1])
+
+    data = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота']
+    time = ['8:00 - 11:20', '11:30 - 15:00', '15:10 - 18:30', '18:45 -  чч:мм']
+
+    sheets = wb.sheetnames
+    sheet = wb[sheets[0]]
+
+    s1 = 1
+    for row in range(1, len(audience) + 1):
+        cell = sheet.cell(row=s1, column=1)
+        value = audience[row-1][0]
+        cell.value = value
+
+        for col in range(2, len(data) + 2):
+            cell2 = sheet.cell(row=s1, column=col)
+            value2 = data[col-2]
+            cell2.value = value2
+
+        for t in range(1, len(time) + 1):
+            cell3 = sheet.cell(row=s1+t, column=1)
+            value3 = time[t-1]
+            cell3.value = value3
+
+        s1 = s1 + 6
+    wb.save('schedule.xlsx')
+
+    def search(name_a):
+        for rowr in sheet.iter_rows():  # Проходимся по строчкам таблицы
+            for cell in rowr:  # Проходимся по ячейкам строчки
+                if cell.value == name_a:  # Проверяем ячейку на наличие аудитории
+                    return cell.row
+
+    for x in group:  # Идем по группам
+        s2 = 0
+        cl = 1
+        for j in data:  # Идем по дням
+            p1 = -1
+            p2 = -1
+            p3 = -1
+            p4 = -1
+            for i in range(2):  # Лимит промежутков в день
+                f = False
+                c = -1
+                while f == False:
+                    c = c + 1
+                    if c < len(audience):
+                        if x[1] <= audience[c][1]:  # Проверяем влезет ли группа в аудиторию
+                            r = search(audience[c][0])
+                            if s2 < len(work):
+                                if audience[c][2] == work[s2][1]:
+                                    if sheet.cell(row=r+1, column=1+cl).value == None:
+                                        p1 = p1 + 1
+                                        if p1 < 1:
+                                            cell4 = sheet.cell(
+                                                row=r+1, column=1+cl)
+                                            value4 = str(
+                                                x[0]) + ' ' + str(work[s2][0])
+                                            cell4.value = value4
+                                            s2 = s2 + 1
+                                            f = True
+                                    elif sheet.cell(row=r+2, column=1+cl).value == None:
+                                        p2 = p2 + 1
+                                        if p2 < 1:
+                                            cell4 = sheet.cell(
+                                                row=r+2, column=1+cl)
+                                            value4 = str(
+                                                x[0]) + ' ' + str(work[s2][0])
+                                            cell4.value = value4
+                                            s2 = s2 + 1
+                                            f = True
+                                    elif sheet.cell(row=r+3, column=1+cl).value == None:
+                                        p3 = p3 + 1
+                                        if p3 < 1:
+                                            cell4 = sheet.cell(
+                                                row=r+3, column=1+cl)
+                                            value4 = str(
+                                                x[0]) + ' ' + str(work[s2][0])
+                                            cell4.value = value4
+                                            s2 = s2 + 1
+                                            f = True
+                                    elif sheet.cell(row=r+4, column=1+cl).value == None:
+                                        p4 = p4 + 1
+                                        if p4 < 1:
+                                            cell4 = sheet.cell(
+                                                row=r+4, column=1+cl)
+                                            value4 = str(
+                                                x[0]) + ' ' + str(work[s2][0])
+                                            cell4.value = value4
+                                            s2 = s2 + 1
+                                            f = True
+                    else:
+                        f = True
+            cl = cl + 1
+        work.append(work[0])
+        work.remove(work[0])
+        wb.save('schedule.xlsx')
 
 
 t1 = User('Admin', "admin")
